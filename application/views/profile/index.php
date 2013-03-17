531849e9063fdc4431b795a205e3ddde6ef9c2e1
SR-sharmeen <sharmeenrehan92@gmail.com>
2013-03-17 14:00:33 +0500

    Update index.php

--- application/views/profile/index.php
+++ application/views/profile/index.php
@@ -1,3 +1,18 @@
+
+<?php  
+
+if($school=="")
+$school="n/a";
+if($university=="")
+$university="n/a";
+if($employer=="")
+$employer="n/a";
+
+
+
+ ?>
+<input type="text" data-provide="typeahead" id="tA" data-source=<?php echo json_encode($arr) ?> />
+
 <div id="coverPhoto" class="coverPhoto">
 
 <img src=<?php echo "".$base."/".$images."/defaultCover.jpg" ?> />
@@ -5,9 +20,42 @@
 <div class="profileWrapper fTop mainBlueColor">
 
 
+
+<div id="infoEditor" class="modal hide fade">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h3>Edit your info</h3>
+  </div>
+  <div class="modal-body">
+    <div>
+	
+<form name="infoEdit" id="infoEdit">
+<label> School 
+<input name="School" type="text" value=<?php echo "".$school."" ?> /></label>
+<label> University
+<input name="University" type="text"    value=<?php echo "".$university."" ?>  /> </label>
+<label> Employer
+<input name="Employer" type="text"   value=<?php echo "".$employer."" ?>  /> </label>
+<label> Birthday 
+<input id="datepicker" type="text" value=<?php echo "".$bday."" ?> name="Bdate"    enabled="false"/></label>
+</form>
+
+</div>
+	
+  </div>
+  <div class="modal-footer">
+    <a href="#" class="btn">Close</a>
+    <a href="#" class="btn btn-primary">Save changes</a>
+  </div>
+</div>
+<!--- -->
+
+
+
+
 <div id="pictureChange" class="modal hide fade in" style="display: none; ">
 <div class="modal-header">
-<a class="close" data-dismiss="modal">×</a>
+<a class="close" data-dismiss="modal">Ã—</a>
 <h3>Profile Pic Panel</h3>
 </div>
 <div class="modal-body">
@@ -72,10 +120,12 @@
 
 
 <div class="personDetailProfile fTop">
+<div id="pro">
+<div id="pictureControl"><a  data-toggle="modal" href="#pictureChange" class="btn btn-primary">Edit Profile Picture</a></div>
+<img class="thumbnail fLeft" id="propic" src=<?php echo "" . $base . "/" . $uploads . "/" . $image_path . ""; ?> /></div>
 
 
-<img class="thumbnail fLeft" id="propic" src=<?php echo "" . $base . "/" . $uploads . "/" . $image_path . ""; ?> />
-<span class="profileInfo well pull-left"><?php echo "<span id='profileInfoTextWrapper'class='profileInfoTextWrapper well pull-left'><p class='profileInfoText'><h3>".$name."</h3>Birthday:".$bday."<br> School:".$school."<br>University".$university."<br>Employer:".$employer."</p></span>" ?>
+<span class="profileInfo well pull-left"><?php echo "<a  data-toggle='modal' href='#infoEditor'><span  id='profileInfoTextWrapper'class='profileInfoTextWrapper well pull-left'><p class='profileInfoText'><h3>".$name."</h3>Birthday:".$bday."<br> School:".$school."<br>University:".$university."<br>Employer:".$employer."</p></span></a>" ?>
 
 <div class="profileOptions fRight">
 <img class="fLeft" src=<?php echo "" . $base . "/" . $images . "/friends-icon.jpg"; ?> />
