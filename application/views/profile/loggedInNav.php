49def893db8723f9736b996e29e0d51cbba9c5fc
Sharmeen Rehan <sharmeenrehan92@gmail.com>
2013-03-22 20:25:53 +0500

    Search Bar Added

--- application/views/profile/loggedInNav.php
+++ application/views/profile/loggedInNav.php
@@ -1,5 +1,7 @@
 <div id="loggedInNav">
-
+<form class="navbar-search pull-left" id="searchProfileHead">
+  <input type="text" class="search-query" placeholder="Search">
+</form>
 <a class="btn" href=<?php echo "".$base."/index.php/profile/logout"?>> Log Out </a>
 
 </div>
