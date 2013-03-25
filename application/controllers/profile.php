dbcc9b7e1921b9dc27f2dae6644449ff87949338
Sharmeen Rehan <sharmeenrehan92@gmail.com>
2013-03-25 21:11:55 +0500

    Search Bar

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -8,6 +8,26 @@ class profile extends CI_Controller {
         $this->load->helper('url');
         $this->load->library('session');
     }
+	
+	public function Search()
+	{
+	 $query=$_POST['SearchBox'];
+	 $resource=$this->main_model->search($query,'firstname');
+
+	}
+	public function updateInfo()
+	{
+		$school=$_POST['school'];
+		$university=$_POST['university'];
+		$employer=$_POST['employer'];
+		$bdate=date('Y-m-d H:i:s', strtotime($_POST['bdate']));
+		$id=$_POST['id'];
+		$data=array('user_id'=>$id,'school'=>$school, 'university'=>$university, 'employer'=>$employer);
+		$this->main_model->insert_sign_up2($data);
+		$blekh=$this->main_model->updateInfo($data);
+		$this->main_model->updateBirthday(array('id'=>$id,'birthday'=>$bdate));
+		$this->index();
+	}
 
     public function cropPicture() {
         $img = $this->session->userdata('img');
