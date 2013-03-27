fc12a2cf9d49cbc401284329cb76c4f3cfa24a5b
AhsanLatif <ahsanlatif18@gmail.com>
2013-03-27 22:14:50 +0500

    none

--- application/views/profile/viewProfile.php
+++ application/views/profile/viewProfile.php
@@ -7,10 +7,6 @@
 
 <div class="profileWrapper fTop mainBlueColor">
 
-
-
-
-
     <div class="personDetailProfile fTop">
 
         <div id="pro">
@@ -18,44 +14,48 @@
 
         <span class="profileInfo well pull-left"><?php echo "<span id='frndprofileInfoTextWrapper'class='frndprofileInfoTextWrapper well pull-left'><p class='frndprofileInfoText'><h3>" . $name . "</h3>Birthday:" . $bday . "<br> School:" . $school . "<br>University" . $university . "<br>Employer:" . $employer . "</p></span>" ?> </span> 
 
-<div class="profileOptions fRight">
- <?php
-        if (isset($fid)) {
-            echo "<div id='pictureControl' class='btn btn-primary'><a href=" . $base . "/index.php/friends/addFriend?fid=" . $fid . "> Add Friend</a></div>";
-        }
-        ?> </span>
-</div>
-
-        <br>
-
-
+        <div class="profileOptions fRight">
+            <?php
+            if (isset($fid)) {
+                echo "<div id='pictureControl' class='btn btn-primary'><a href=" . $base . "/index.php/friends/addFriend?fid=" . $fid . "> Add Friend</a></div>";
+            }
+            ?> </span>
+        </div>
     </div>
+    <div id="viewFriends"class="modal hide fade">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h3>Your Friends!</h3>
+        </div>
+        <div class="modal-body">
+            <div class="GalleryImage">
+                <a target="_blank" href="klematis_big.htm">
+                    <img src="http://fellowshipofminds.files.wordpress.com/2012/04/mickey-mouse-mickey-mouse-29454673-1024-768.jpg" alt="Klematis" width="110" height="90">
+                </a>
+                <div class="GalleryCaption">Person's Name</div>
+            </div>
 
-
-
-        <div class="nothing">
+        </div>
+        <div class="modal-footer">
+            <a href="#" data-dismiss="modal" class="btn">Close</a>
         </div>
     </div>
 
 
-
-
-
-        <?php
-        if (isset($fid)) {
-            echo "<div id='pictureControl' class='btn btn-primary'><a href=" . $base . "/index.php/friends/addFriend?fid=" . $fid . "> Add Friend</a></div>";
-        }
-        ?>
-
-    <div class="wall fLeft">
-        <h1> Friends! </h1>
-        <?php
-        foreach ($friends as $friend) {
-            echo "<a href=" . $base . "/index.php/profile/viewProfile?id=" . $friend['friend_id'] . ">" . $friend['friend_first_name'] . "</a>";
-            echo '</br>';
-        }
-        ?>
+    <div class="nothing">
     </div>
+</div>
+
+
+<div class="wall fLeft">
+    <h1> Friends! </h1>
+    <?php
+    foreach ($friends as $friend) {
+        echo "<a href=" . $base . "/index.php/profile/viewProfile?id=" . $friend['friend_id'] . ">" . $friend['friend_first_name'] . "</a>";
+        echo '</br>';
+    }
+    ?>
+</div>
 
 
 </div>
