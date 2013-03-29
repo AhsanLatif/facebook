d7aa4ff8ab87bdb496cde7b5a93ed902c7d82a87
AhsanLatif <ahsanlatif18@gmail.com>
2013-03-30 00:29:47 +0500

    loogedin

--- application/views/profile/loggedInNav.php
+++ application/views/profile/loggedInNav.php
@@ -1,32 +1,9 @@
 <div id="loggedInNav">
 
-<div  id="pendingRequests" class="dropdown">
-  <img data-toggle="dropdown" src=<?php echo "" . $base . "/" . $images . "/friends-icon.gif" ?> />
-  <ul class="dropdown-menu pull-left">
-    <li><a href="#">Action</a></li>
-    <li><a  href="#">Another action</a></li>
-    <li><a  href="#">   </a></li>
-	 <li><a  href="#">   </a></li>
-	  <li><a  href="#">   </a></li>
-   
-  </ul>
-</div>
-<div class="dropdown" id="notifications">
-  <button data-toggle="dropdown" > ! </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu" >
-  <span id="noList"> </span>
-	  <li><a  href="#">   </a></li>
-	 <li><a  href="#">   </a></li>
-	  <li><a  href="#">   </a></li>
-   
-  </ul>
-</div>
-<input type="hidden" name='currid' id='currid' value=<?php echo $id ?> />
-<input type="hidden" name="path" id="path"  value=<?php echo "".$base."/index.php/profile"; ?> />
-
-  <div id="searchButton"><a onclick="searchProfileHead.submit();"><img src=<?php echo "" . $base . "/" . $images . "/search.png"; ?> /> </a></div>
-
-           <?php
+    <div  id="pendingRequests" class="dropdown">
+        <img data-toggle="dropdown" src=<?php echo "" . $base . "/" . $images . "/friends-icon.gif" ?> />
+        <ul class="dropdown-menu pull-left">
+            <?php
             if (isset($requests)) {
                 foreach ($requests as $request) {
                     echo "<li><a href=" . $base . "/index.php/profile/viewProfile?id=" . $request['user_id'] . ">" . $request['friend_first_name'] . "</a></li>";
@@ -34,19 +11,21 @@
                 }
             }
             ?>
-
         </ul>
     </div>
     <div class="dropdown" id="notifications">
         <button data-toggle="dropdown" > ! </button>
         <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu" >
-            <li id="noList"><a href="#"> </li>
+            <span id="noList"> </span>
             <li><a  href="#">   </a></li>
             <li><a  href="#">   </a></li>
             <li><a  href="#">   </a></li>
 
         </ul>
     </div>
+    <input type="hidden" name='currid' id='currid' value=<?php echo $id ?> />
+    <input type="hidden" name="path" id="path"  value=<?php echo "" . $base . "/index.php/profile"; ?> />
+
     <div id="searchButton"><a onclick="searchProfileHead.submit();"><img src=<?php echo "" . $base . "/" . $images . "/search.png"; ?> /> </a></div>
 
     <?php
@@ -64,7 +43,7 @@
         <input type="text" name="SearchBox" id="search" data-items="4" class="search-query" placeholder="Search" data-provide="typeahead" data-source=<?php echo json_encode($frends); ?> autocomplete="off" />
     </form>
 
-<div id="clearfix"></div>
-<div id="logOut"><a href=<?php echo "".$base."/index.php/profile/logout"?>><img src=<?php echo "".$base."/".$images."/logout_button.gif" ?> class="fTop" /></a></div>
+    <div id="clearfix"></div>
+    <div id="logOut"><a href=<?php echo "" . $base . "/index.php/profile/logout" ?>><img src=<?php echo "" . $base . "/" . $images . "/logout_button.gif" ?> class="fTop" /></a></div>
 
 </div>
