e80c2de54d5bec6b940f4fa8abceff1f0d0153b9
Sharmeen Rehan <sharmeenrehan92@gmail.com>
2013-04-19 19:49:38 +0500

    and more...

--- application/views/profile/loggedInNav.php
+++ application/views/profile/loggedInNav.php
@@ -1,36 +1,3 @@
-
-<div id="loggedInNav">
-
-<div  id="pendingRequests" class="dropdown">
-  <img data-toggle="dropdown" src=<?php echo "" . $base . "/" . $images . "/friends-icon.gif" ?> />
-  <ul class="dropdown-menu pull-left">
-            <?php
-            if (isset($requests)) {
-                foreach ($requests as $request) {
-                    echo "<li><a href=" . $base . "/index.php/profile/viewProfile?id=" . $request['user_id'] . ">" . $request['friend_first_name'] . "</a></li>";
-                    echo '</br>';
-                }
-            }
-            ?>
-        </ul>
-</div>
-<div class="dropdown" id="notifications">
-  <button data-toggle="dropdown" > ! </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu" >
-  <span id="noList"> 
-
-  
-  </span>
-	  <li><a  href="#">   </a></li>
-	 <li><a  href="#">   </a></li>
-	  <li><a  href="#">   </a></li>
-   
-  </ul>
-</div>
-<input type="hidden" name='currid' id='currid' value=<?php echo $id ?> />
-<input type="hidden" name="path" id="path"  value=<?php echo "".$base."/index.php/profile"; ?> />
-  <div id="searchButton"><a onclick="searchProfileHead.submit();"><img src=<?php echo "" . $base . "/" . $images . "/search.png"; ?> /> </a></div>
-
 <?php
 if(isset($friends))
 {
@@ -45,11 +12,46 @@ if(isset($friends))
 		else{
 		$frends="";}
 		?>
-<form class="fTop"class="navbar-search pull-left" method="post"id="searchProfileHead" action=<?php echo "".$base."/index.php/profile/Search" ?> >
+<div id="loggedInNav">
+
+<div  id="pendingRequests" class="dropdown">
+  <img data-toggle="dropdown" src=<?php echo "" . $base . "/" . $images . "/friends-icon.gif"; ?> />
+  <ul class="dropdown-menu"> 
+
+            <?php
+            if (isset($requests)) {
+                foreach ($requests as $request): {
+                    echo "<li><a href="."" . $base . "/index.php/profile/viewProfile?id=" . $request['user_id'] ."". ">" . $request['friend_first_name'] . "</a></li>";
+                    
+                }endforeach;
+            }
+			else
+			{
+			echo "  <li> No Requests </li>";
+			}
+            ?>
+        </ul>
+</div>
+<div class="dropdown" id="notifications">
+  <button data-toggle="dropdown" > ! </button>
+  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu" >
+<span id="noList">
+</span>
+   
+  </ul>
+</div>
+<input type="hidden" name='currid' id='currid' value=<?php echo $id ?> />
+<input type="hidden" name="path" id="path"  value=<?php echo "".$base."/index.php/profile"; ?> />
+  <div id="searchButton"><a onclick="searchProfileHead.submit();"><img src=<?php echo "" . $base . "/" . $images . "/search.png"; ?> /> </a></div>
+
+
+<form class="navbar-search pull-left" method="post" id="searchProfileHead" action=<?php echo "".$base."/index.php/profile/Search" ?> >
   <input type="text" name="SearchBox" id="search" data-items="4" class="search-query" placeholder="Search" data-provide="typeahead" data-source=<?php echo json_encode($frends); ?> autocomplete="off" />
   </form>
 
 <div id="clearfix"></div>
+
+
 <div id="logOut">
 
 <div class="btn-group">
@@ -58,15 +60,11 @@ if(isset($friends))
     <span class="caret"></span>
   </a>
   <ul class="dropdown-menu">
-	    <li ><a href=<?php echo "".$base."/index.php/profile/goToPage/newsfeed";?>>View Newsfeed </a></li>
+	    <li ><a href=<?php echo "".$base."/index.php/profile/goToPage/newsfeed";  ?> >View Newsfeed </a></li>
 		<li> <a href=<?php echo "".$base."/index.php/profile/logout"; ?>> Logout </a> </li>
-	<li><a  href="#">   </a></li>
-	 <li><a  href="#">   </a></li>
-	  <li><a  href="#">   </a></li>
-	  	<li><a  href="#">   </a></li>
-	 <li><a  href="#">   </a></li>
-	  <li><a  href="#">   </a></li>
+
   </ul>
-</div></div>
+</div>
+</div>
 
 </div>
