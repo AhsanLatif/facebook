91b1eaed7fffd2c3ebd70fcf3aba8d7b664791a9
AhsanLatif <ahsanlatif18@gmail.com>
2013-04-19 11:50:35 +0500

    config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/webProject';
+$config['base_url']	= 'http://localhost/facebook';
 $config['images']='media/images';
 /*
 |--------------------------------------------------------------------------
