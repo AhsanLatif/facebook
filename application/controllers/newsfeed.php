2c0b9875b4856900a852249e3fc2f5f14a370522
AhsanLatif <ahsanlatif18@gmail.com>
2013-04-22 07:36:11 +0500

    video

--- application/controllers/newsfeed.php
+++ application/controllers/newsfeed.php
@@ -1,59 +1,81 @@
 <?php
+
 class Newsfeed extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
         $this->load->model('main_model');
         $this->load->helper('url');
-		$this->load->helper('dom');
-     $this->load->library('session');
+//        $this->load->helper('dom');
+        $this->load->library('session');
     }
 
+    public function index() {
+        // Grab HTML From the URL
+        /*
+          $html = file_get_html('http://codeigniter.com/');
 
-	public function index()
-	
-	{
-	// Grab HTML From the URL
-/*
-$html = file_get_html('http://codeigniter.com/');
 
- 
-// find all link on Codeigniter Site
+          // find all link on Codeigniter Site
 
-foreach($html->find('img') as $e)
-echo $e . '<br>';*/
+          foreach($html->find('img') as $e)
+          echo $e . '<br>'; */
 
-	$data=$this->main_model->load_media();
-	$this->load->view('header',$data);
-	$this->load->view('newsFeed/index',$data);
-	}
-	public function uploadPhoto()
-	{
-		$text=$_POST['PicText'];
-	    $config['upload_path'] = './uploads/';
+        $data = $this->main_model->load_media();
+        $this->load->view('header', $data);
+        $this->load->view('newsFeed/index', $data);
+    }
+
+    public function uploadPhoto() {
+        $text = $_POST['PicText'];
+        $config['upload_path'] = './uploads/';
         $config['allowed_types'] = 'gif|jpg|png';
         $config['max_size'] = '10000';
         $config['max_width'] = '10240';
         $config['max_height'] = '7680';
 
         $this->load->library('upload', $config);
-       if( $this->upload->do_upload()){
-        $imgdata = $this->upload->data();
-		$filename = $imgdata['file_name'];
-		$this->addPost($text,$filename,'1');}
-	}
-	
-	public function addPost($content, $link, $type)
-	{
-		$id=$this->session->userdata('id');
-		$this->main_model->addPost($id,$content,$link,$type);
-	}
-	public function getPosts($iter)
-	{
-	
-		$id=$this->session->userdata('id');
-		
-		echo json_encode($this->main_model->getPosts($id,$iter));
-	}
-	
+        if ($this->upload->do_upload()) {
+            $imgdata = $this->upload->data();
+            $filename = $imgdata['file_name'];
+            $this->addPost($text, $filename, '1');
+        }
+    }
+
+    public function addPost($content, $link, $type) {
+        $id = $this->session->userdata('id');
+        $this->main_model->addPost($id, $content, $link, $type);
+    }
+
+    public function getPosts($iter) {
+
+        $id = $this->session->userdata('id');
+
+        echo json_encode($this->main_model->getPosts($id, $iter));
+    }
+
+    public function add_video() {
+        if (isset($_FILES['video']['name']) && $_FILES['video']['name'] != '') {
+            unset($config);
+            $date = date("ymd");
+            $configVideo['upload_path'] = './video';
+            $configVideo['max_size'] = '10240';
+            $configVideo['allowed_types'] = 'avi|flv|wmv|mp3';
+            $configVideo['overwrite'] = FALSE;
+            $configVideo['remove_spaces'] = TRUE;
+            $video_name = $date . $_FILES['video']['name'];
+            $configVideo['file_name'] = $video_name;
+
+            $this->load->library('upload', $configVideo);
+            $this->upload->initialize($configVideo);
+            if (!$this->upload->do_upload('video')) {
+                echo $this->upload->display_errors();
+            } else {
+                $videoDetails = $this->upload->data();
+                echo "Successfully Uploaded";
+            }
+        }
+
+    }
 }
+        
--- application/views/newsfeed/index.php
+++ application/views/newsfeed/index.php
@@ -1,60 +1,73 @@
 <div id="viewLargePic"class="modal hide fade">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-			
-            <h3>Picture Viewer </h3>
-	        </div>
-        
-		<div class="modal-body">
-		<input type="hidden" id="clickedPic" />
-		<img id="picLargeView" src="" alt="picture" width='250' height='400'/>
-        </div>
-        <div class="modal-footer">
-            <a href="#" data-dismiss="modal" class="btn">Close</a>
-        </div>
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+
+        <h3>Picture Viewer </h3>
     </div>
+
+    <div class="modal-body">
+        <input type="hidden" id="clickedPic" />
+        <img id="picLargeView" src="" alt="picture" width='250' height='400'/>
+    </div>
+    <div class="modal-footer">
+        <a href="#" data-dismiss="modal" class="btn">Close</a>
+    </div>
+</div>
 
 <!-- body -->	
 <div class="fTop">
-			<div class="tabbable"> <!-- Only required for left/right tabs  -->
-                <ul class="nav nav-tabs">
-                    <li class="active"><a href="#tab1" data-toggle="tab">Status/Link</a></li>
-                    <li><a href="#tab2" data-toggle="tab">Photo</a></li>
-                    <li><a href="#tab3" data-toggle="tab">Video</a></li>
-                </ul>
-			</div>
-			<div id="clearfix"></div>
-  <div class="tab-content" >
-                    <div class="tab-pane active" id="tab1">
-						<input type="text" name="statusPost" id="statusPost" />
-						<input type="submit" class="btn" value="post" id="simplePost" />
-					</div>
-				 
-                    <div class="tab-pane" id="tab2">
-					<div class="collapse in" id="dropper" >
-					
-					<label> Drop Photos here or click to upload </label>
-					  <form action=<?php echo "".$base."/index.php/newsfeed/uploadPhoto"; ?> class="dropzone"id="dropFiles">
-					  
-					 <label>Caption </label><input type="text" id='PicText' placeholder="Add a caption before you drop your photo!" name="PicText" />
-					</form>
-					</div>
-					<div id="normal-toggle-button">
+    <div class="tabbable"> <!-- Only required for left/right tabs  -->
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#tab1" data-toggle="tab">Status/Link</a></li>
+            <li><a href="#tab2" data-toggle="tab">Photo</a></li>
+            <li><a href="#tab3" data-toggle="tab">Video</a></li>
+        </ul>
+    </div>
+    <div id="clearfix"></div>
+    <div class="tab-content" >
+        <div class="tab-pane active" id="tab1">
+            <input type="text" name="statusPost" id="statusPost" />
+            <input type="submit" class="btn" value="post" id="simplePost" />
+        </div>
 
-					 <input type="button" value="drop" data-toggle="collapse" data-target="#dropper" id="doneDragging" />
-					</div>
-                    </div>		
-			
-   </div>
-   <hr>
-<input type="hidden" id="currId" value="0" />
-   <div id="theWall">
-   
-	<div id="newPostAdder" ></div>
-   
-   </div>
-   </div>
-   <div id="clearfix">
-   </div>
-		
-		
+        <div class="tab-pane" id="tab2">
+            <div class="collapse in" id="dropper" >
+
+                <label> Drop Photos here or click to upload </label>
+                <form action=<?php echo "" . $base . "/index.php/newsfeed/uploadPhoto"; ?> class="dropzone"id="dropFiles">
+
+                    <label>Caption </label><input type="text" id='PicText' placeholder="Add a caption before you drop your photo!" name="PicText" />
+                </form>
+            </div>
+            <div id="normal-toggle-button">
+
+                <input type="button" value="drop" data-toggle="collapse" data-target="#dropper" id="doneDragging" />
+            </div>
+        </div>	
+
+        <div class="tab-pane" id="tab3">
+            <form class="cssform" name="property" id="property" method="POST" action="<?php echo base_url() ?>index.php/newsfeed/add_video"  enctype="multipart/form-data" >
+                <table>
+                    <tr>
+                        <td>Select Video :</td>
+                        <td><input type="file" id="video" name="video" ></td>
+                    </tr>
+                    <tr>
+                        <td> <input type="submit" class="btn" id="button" name="submit" value="Submit" /></td>
+                    </tr>
+                </table>
+            </form>
+        </div>	
+
+    </div>
+    <hr>
+    <input type="hidden" id="currId" value="0" />
+    <div id="theWall">
+
+        <div id="newPostAdder" ></div>
+
+    </div>
+</div>
+<div id="clearfix">
+</div>
+
